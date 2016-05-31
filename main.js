eb0a199c9125f7da4813eaf2bbc8c045fa45d11d
kla12 <kla12@uw.edu>
2016-05-30 17:32:26 -0700

    Update main.js

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ Carson.prototype.update = function () {
 		this.animation = new Animation(AM.getAsset("./img/carsonright.png"), 49, 48.2, 3, .1, 3, true, size);
 		dir = 0;
 	}
-    	if (this.x > 1800) {
+    	if (this.x > 1400) {
 		//this.spritesheet = AM.getAsset("./img/carsonleft.png");
 		this.animation = new Animation(AM.getAsset("./img/carsonleft.png"), 49, 48.2, 3, .1, 3, true, size);
 		dir = 1;
